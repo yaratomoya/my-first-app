0a2c29c4b253a3d1ed09c986fe21dea40c39d57a
User <User@H27-NotePC06>
2015-05-13 15:52:49 +0900

    フォームとセッションを記述(ちょっと足りない)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,18 +3,22 @@ package controllers;
 import java.util.*;
 
 import models.*;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
 public class Application extends Controller {
+	public static Form<Task> taskForm = Form.form(Task.class);
 
     public static Result index() {
+    	System.out.println(flash("foo"));
         return ok(index.render("Your new application is ready."));
     }
 
     public static Result tasks(){
     	List<String> taskList=Arrays.asList("foo", "bar", "baz", "<strong>Pizza</strong>", "<script>alert('hello');</script>");
+    	flash("foo", "pizza");
     	return ok(tasks.render(taskList));
     }
 
@@ -36,7 +40,18 @@ public class Application extends Controller {
     	Integer cnt=taskList.size();
     	List<Task> taskList2=Task.find.where().eq("name", "ピザを5枚食べる").findList();
 
-    	return ok(addrec.render(taskList, now, cnt, taskList2));
+    	return ok(addrec.render(taskList, now, cnt, taskList2, taskForm));
+    }
+
+    public static Result createTask(){
+    	Form<Task> form = Form.form(Task.class).bindFromRequest();
+    	if(form.hasErrors()){
+    		return badRequest(form.errorsAsJson());
+    	}else{
+    		Task newTask=form.get();
+    		newTask.save();
+    		return redirect(routes.Application.addrec());
+    	}
     }
 
 }
--- app/models/Task.java
+++ app/models/Task.java
@@ -1,13 +1,17 @@
 package models;
 
+import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
+
 import javax.persistence.*;
+
 import java.util.*;
 
 @Entity
 public class Task extends Model {
 	@Id
 	public Integer id;
+	@Required
 	public String name;
 	public Date period;
 
