db85fe90672157a3126912f0c13b977c23b6804d
Administrator <Administrator@user-pc>
2015-05-19 23:41:55 +0900

    それっぽい動きのログイン画面

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -1,13 +1,23 @@
 package controllers;
 
-import play.mvc.Result;
+import play.mvc.*;
+import play.data.*;
+import views.html.authentication.*;
 
-public class Authentication {
+public class Authentication extends Controller{
+	public static class Login{
+		public String username;
+		public String password;
+	}
+
+	public static Form<Login> loginForm=Form.form(Login.class);
+
 	public static Result index(){
-		return TODO;
+		return ok(index.render(loginForm));
 	}
 
 	public static Result authenticate(){
-		return TODO;
+		Login login=loginForm.bindFromRequest().get();
+		return ok("ログインしたユーザは" + login.username + "です。");
 	}
 }
