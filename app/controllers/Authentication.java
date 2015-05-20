bd3439d12055a35da713d2b60a376fa04a56e9af
Administrator <Administrator@user-pc>
2015-05-21 02:51:02 +0900

    ログイン途中

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -8,16 +8,42 @@ public class Authentication extends Controller{
 	public static class Login{
 		public String username;
 		public String password;
+
+		public String validate(){
+			if(authenticate(username, password)){
+				return null;
+			}
+			return "Invalid username and password";
+		}
+
+		private Boolean authenticate(String username, String password){
+			return (username.equals("yara") && password.equals("tomo4522"));
+		}
 	}
 
 	public static Form<Login> loginForm=Form.form(Login.class);
 
 	public static Result index(){
+		if(session("login") != null){
+			return ok("あなたは既に" + session("login") + "としてログインしています。");
+		}
 		return ok(index.render(loginForm));
 	}
 
 	public static Result authenticate(){
-		Login login=loginForm.bindFromRequest().get();
-		return ok("ログインしたユーザは" + login.username + "です。");
+		Form<Login> form=loginForm.bindFromRequest();
+
+		if(form.hasErrors()){
+			return badRequest(index.render(form));
+		}else{
+			Login login=form.get();
+			session("login", login.username);
+			return ok("ようこそ" + login.username + "さん!!");
+		}
+	}
+
+	public static Result logout(){
+		session().clear();
+		return redirect(routes.Authentication.index());
 	}
 }
